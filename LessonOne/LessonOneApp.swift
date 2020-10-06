8f0836bf943d2fd68e54c121cf25c72fe5d1f7c0
Jeffrey Wang <jeffreywangcf@gmail.com>
2020-10-06 06:16:03 -0500

    Initial Commit

---
+++ LessonOne/LessonOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonOneApp.swift
+//  LessonOne
+//
+//  Created by Jeffery Wang on 10/6/20.
+//
+
+import SwiftUI
+
+@main
+struct LessonOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
